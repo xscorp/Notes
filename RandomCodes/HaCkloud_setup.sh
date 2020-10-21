953ce871179ecd50806f159ce2e56d8b27610da7
xscorp <37046662+xscorp@users.noreply.github.com>
2020-10-21 12:12:01 +0530

    Update HaCkloud_setup.sh

--- RandomCodes/HaCkloud_setup.sh
+++ RandomCodes/HaCkloud_setup.sh
@@ -12,18 +12,21 @@ apt-get update
 #============================#
 echo "[#] Installing necessary utils"
 #============================#
-echo "[*] Enabling 'main' and 'universal' repository"
+echo "	[*] Enabling 'main' and 'universal' repository"
 sudo add-apt-repository main
 sudo add-apt-repository universe
 
-echo "[*] Installing unzip"
+echo "	[*] Installing unzip"
 apt install unzip
 
-echo "[*] Installing golang-go"
+echo "	[*] Installing golang-go"
 apt install golang-go
 
-echo "[*] Installing python3-pip"
+echo "	[*] Installing python3-pip"
 apt install python3-pip
+
+echo "	[*] Installing ruby"
+apt install ruby
 
 #============================#
 echo "[+] Installing apktool"
@@ -77,3 +80,48 @@ git clone https://github.com/aboul3la/Sublist3r.git /usr/share/Sublist3r
 pip3 install -r /usr/share/Sublist3r/requirements.txt
 echo 'cd /usr/share/Sublist3r/; python3 sublist3r.py $@; cd - 1>/dev/null;' > /usr/local/bin/sublist3r
 chmod +x /usr/local/bin/sublist3r
+
+#============================#
+echo "[+] Installing Subfinder"
+#============================#
+cd /tmp/
+wget https://github.com/projectdiscovery/subfinder/releases/download/v2.4.5/subfinder_2.4.5_linux_amd64.tar.gz -O /tmp/subfinder.tar.gz
+tar -xvzf /tmp/subfinder.tar.gz
+mv subfinder /usr/local/bin/
+chmod +x /usr/local/bin/subfinder
+cd - 1>/dev/null
+
+
+#============================#
+echo "[+] Installing Subfinder"
+#============================#
+sudo apt-get install wpscan
+
+#============================#
+echo "[+] Installing Nuclei"
+#============================#
+cd /tmp
+wget https://github.com/projectdiscovery/nuclei/releases/download/v2.1.1/nuclei_2.1.1_linux_amd64.tar.gz -O /tmp/nuclei.tar.gz
+tar -xvzf nuclei.tar.gz
+mv nuclei /usr/local/bin
+chmod +x /usr/local/bin
+cd - 1>/dev/null
+
+
+#============================#
+echo "[+] Installing Naabu"
+#============================#
+cd /tmp
+wget https://github.com/projectdiscovery/naabu/releases/download/v2.0.2/naabu_2.0.2_linux_amd64.tar.gz -O naabu.tar.gz
+tar -xvzf naabu.tar.gz
+mv naabu /usr/local/bin/
+chmod +x /usr/local/bin/naabu
+cd - 1>/dev/null
+
+
+#============================#
+echo "[+] Installing filter-resolved"
+#============================#
+go get github.com/tomnomnom/hacks/filter-resolved
+mv ~/go/bin/filter-resolved /usr/local/bin/
+rm -rf ~/go
