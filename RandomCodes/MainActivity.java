7f7915c6ceb7c1d681bd9bf3992c729437535a37
xscorp <37046662+xscorp@users.noreply.github.com>
2021-02-01 13:48:07 +0530

    Add files via upload

---
+++ RandomCodes/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.newrequestproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void SendTheRequest(View view)
+    {
+        mTextView = findViewById(R.id.midoutput);
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://pastebin.com/raw/mvFXpkPt";
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(response.isSuccessful())
+                {
+                    final String myResponse = response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextView.setText(myResponse);
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
